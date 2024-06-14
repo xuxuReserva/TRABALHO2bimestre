5c0773f7e963248f40469ab40d237d30160a2805
xuxuReserva <xuxuescola@gmail.com>
2024-06-14 12:58:23 +0000

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,6 +4,16 @@ import { BlurView } from 'react-native-blur';
 
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 
+const fazerChamada = async () => {
+  // Exemplo de uso
+  const numero = "18991207281"; 
+  const url = `tel:${numero}`;
+  try {
+    await Linking.openURL(url);
+  } catch (error) {
+    console.error("Erro ao abrir o cliente de telefone:", error);
+  }
+};
 
 export default function HomeScreen() {
   const openUrl = async (url) => {
@@ -19,10 +29,12 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/perfil.jpeg')}
+          source={require('@/assets/images/fundo.jpg')}
           style={styles.profileImage}
         />
+
       }
+      
       renderHeader={() => (
         <BlurView
           style={styles.blurContainer}
@@ -34,7 +46,18 @@ export default function HomeScreen() {
       )}
     >
 
-      <Text style={styles.title}>Bem-vindo!</Text>   
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+          <View style={{}}>
+          <Image
+          source={require('@/assets/images/perfil2.jpeg')}
+          style={{width: 100, height: 100, borderRadius: 100,  }} 
+        />
+          </View>
+        </View>
+
+      <Text style={styles.title}>João Vitor Dutra Macedo</Text>
+          <Text style={styles.title}>_______________________________</Text>  
+          <Text style={styles.texto}>Estudante</Text>
 
       <View style={styles.container}>
         <TouchableOpacity
@@ -47,19 +70,46 @@ export default function HomeScreen() {
 
       <View>
       <TouchableOpacity
-          onPress={() => openUrl('https://www.facebook.com/')}
+          onPress={() => openUrl('instagram://user?username=joaovitordutramacedo')}
           style={styles.button}
         >
-          <Text style={styles.buttonText}>Facebook</Text>
+          <Text style={styles.buttonText}>Instagram</Text>
         </TouchableOpacity>
       </View>
 
       <View>
       <TouchableOpacity
-          onPress={() => openUrl('https://www.facebook.com/')}
+          onPress={() => openUrl('https://wa.me/5518991207281')}
           style={styles.button}
         >
-          <Text style={styles.buttonText}>Facebook</Text>
+          <Text style={styles.buttonText}>Whatsapp</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+      <TouchableOpacity
+          onPress={() => openUrl('https://www.linkedin.com/in/jo%C3%A3o-vitor-dutra-macedo-989b53313/')}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Linkedin</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+      <TouchableOpacity
+          onPress={() => openUrl('https://github.com/xuxuReserva')}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>GitHub</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+      <TouchableOpacity
+          onPress={fazerChamada}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Telefone</Text>
         </TouchableOpacity>
       </View>
      
@@ -71,10 +121,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  perfil: {
+    borderRadius: 10,
+    width: 22,
+    height: 22,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
   profileImage: {
     width: 395,
     height: 250,
-    resizeMode: 'cover',
+    marginHorizontal: 10,
+    resizeMode: "repeat",
+    
   },
   blurContainer: {
     flex: 1,
@@ -85,6 +145,14 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     color: '#fff',
+    textAlign: "center"
+  },
+  texto: {
+    fontSize: 20,
+    color: '#fff',
+    textAlign: "center",
+    fontWeight: 'bold'
+    
   },
   buttonContainer: {
     flexDirection: 'row',
